f05acdd3e2b54376a20cad9c4ef748db19cb7942
MDGcode <adrianboscanici@yahoo.com>
2024-05-03 18:53:11 +0300

    added dark and light mode

--- app/recipes/[id].tsx
+++ app/recipes/[id].tsx
@@ -35,42 +35,58 @@ export default function RecipePage() {
 
   // Function to strip HTML tags from a string
   const stripHtmlTags = (html: string) => {
-    return html.replace(/<[^>]*>?/gm, "");
+    return html.replace(/<[^>]*>?/gm, " ");
   };
 
   return (
-    <ScrollView className="absolute h-[100%] bg-green-500 -z-10 w-full">
-      <View className="flex justify-between items-center bg-green-500">
+    <ScrollView className="absolute h-[100%] bg-gray-200 dark:bg-slate-800 -z-10 w-full">
+      <View className="flex justify-between items-center bg-gray-200 dark:bg-slate-800 ">
         {recipe && (
           <View className="flex justify-between items-center">
-            <Text className="mt-12 mb-4 text-2xl font-semibold text-center">
+            <Text className="mt-12 mb-4 text-2xl font-semibold text-center dark:text-slate-300">
               {recipe.title}
             </Text>
             <View className="flex justify-between items-center">
               <Image
                 source={{ uri: recipe.image }}
                 style={{ width: 300, height: 300 }}
-                className="rounded-xl"
+                className="rounded-xl border-2 border-black"
               />
             </View>
-            <Text className="mb-12 text-sm">Source: {recipe.sourceName}</Text>
-            <Text className="font-semibold text-xl">Summary</Text>
+            <Text className="mb-12 text-sm dark:text-slate-300">
+              Source: {recipe.sourceName}
+            </Text>
+            <Text className="font-semibold text-xl dark:text-slate-300">
+              Summary
+            </Text>
             <ScrollView>
-              <Text className=" text-xs p-4">
+              <Text className=" text-xs p-4 dark:text-slate-300">
                 {stripHtmlTags(recipe.summary)}
               </Text>
             </ScrollView>
-            <Text className="font-semibold text-xl">Ingredients</Text>
+            <Text className="font-semibold text-xl dark:text-slate-300">
+              Ingredients
+            </Text>
             <View className="py-4">
               {recipe.extendedIngredients.map((ingredient, index) => (
-                <Text key={index} className="text-sm ml-[10px]">
-                  <AntDesign name="arrowright" size={10} color="black" />{" "}
+                <Text
+                  key={index}
+                  className="text-sm ml-[10px] dark:text-slate-300"
+                >
+                  <AntDesign
+                    name="arrowright"
+                    size={10}
+                    color="gray"
+                    className=""
+                  />{" "}
                   {ingredient.original}
                 </Text>
               ))}
             </View>
-            <Text className="font-semibold text-xl">Instructions</Text>
-            <Text className=" text-sm p-4">
+            <Text className="font-semibold text-xl dark:text-slate-300">
+              Instructions
+            </Text>
+            <Text className=" text-sm p-4 dark:text-slate-300">
               {stripHtmlTags(recipe.instructions)}
             </Text>
           </View>
